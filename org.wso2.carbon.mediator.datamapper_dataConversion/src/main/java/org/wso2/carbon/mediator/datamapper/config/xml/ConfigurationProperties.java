a6df8c423d308e2fd4027788ea9902fe8a4e450c
sohaniwso2 <sohani@wso2.com>
2014-04-08 23:27:40 +0530

    DataMapperMediator with Type conversion

---
+++ org.wso2.carbon.mediator.datamapper_dataConversion/src/main/java/org/wso2/carbon/mediator/datamapper/config/xml/ConfigurationProperties.java
@@ -0,0 +1,10 @@
+package org.wso2.carbon.mediator.datamapper.config.xml;
+
+public class ConfigurationProperties {
+	public static String DATAMAPPER = "datamapper";
+	public static String CONFIG = "config";
+	public static String INPUTSCHEMA = "inputSchema";
+	public static String OUTPUTSCHEMA = "outputSchema";
+	public static String INPUTTYPE = "inputType";
+	public static String OUTPUTTYPE = "outputType";
+}
---
+++ org.wso2.carbon.mediator.datamapper_dataConversion/src/main/java/org/wso2/carbon/mediator/datamapper/config/xml/DataMapperMediatorFactory.java
@@ -0,0 +1,85 @@
+package org.wso2.carbon.mediator.datamapper.config.xml;
+
+import org.apache.axiom.om.OMAttribute;
+import org.apache.axiom.om.OMElement;
+import org.apache.synapse.Mediator;
+import org.apache.synapse.config.xml.*;
+import org.apache.synapse.mediators.Value;
+import org.wso2.carbon.mediator.datamapper.DataMapperMediator;
+
+import javax.xml.namespace.QName;
+
+import java.util.Properties;
+
+/*Configuration syntax:
+ * <datamapper config="gov:datamapper/mappingConfig.dmc"
+ *   inputSchema="gov:datamapper/inputSchema.avsc"
+ *   outputSchema="gov:datamapper/outputSchema.avsc"/> 
+ */
+
+public class DataMapperMediatorFactory extends AbstractMediatorFactory {
+
+	/*private static final String CONFIG_ATTRIBUTE = Messages.DataMapperMediatorFactory_configAttribute;
+	private static final String INPUTSCHEMA_ATTRIBUTE = Messages.DataMapperMediatorFactory_inputSchemaAttribute;
+	private static final String OUTPUTSCHEMA_ATTRIBUTE = Messages.DataMapperMediatorFactory_outputSchemaAttribute;*/
+
+	private static final QName TAG_QNAME = new QName(XMLConfigConstants.SYNAPSE_NAMESPACE,
+			ConfigurationProperties.DATAMAPPER);
+
+	@Override
+	public QName getTagQName() {
+		return TAG_QNAME;
+	}
+
+	@Override
+	protected Mediator createSpecificMediator(OMElement element, Properties properties) {
+
+		DataMapperMediator datamapperMediator = new DataMapperMediator();
+
+		OMAttribute configKeyAttribute = element.getAttribute(new QName(
+				ConfigurationProperties.CONFIG));
+		OMAttribute inputSchemaKeyAttribute = element.getAttribute(new QName(
+				ConfigurationProperties.INPUTSCHEMA));
+		OMAttribute outputSchemaKeyAttribute = element.getAttribute(new QName(
+				ConfigurationProperties.OUTPUTSCHEMA));
+		OMAttribute inputTypeAttribute = element.getAttribute(new QName(
+				ConfigurationProperties.INPUTTYPE));
+		OMAttribute outputTypeAttribute = element.getAttribute(new QName(
+				ConfigurationProperties.OUTPUTTYPE));
+
+		/*
+		 * ValueFactory for creating dynamic or static Value and provide methods
+		 * to create value objects
+		 */
+		ValueFactory keyFac = new ValueFactory();
+
+		if (configKeyAttribute != null) {
+			// Create dynamic or static key based on OMElement
+			Value configKeyValue = keyFac.createValue(configKeyAttribute.getLocalName(), element);
+			// set key as the Value
+			datamapperMediator.setConfigurationKey(configKeyValue);
+		} else {
+		//	handleException(CONFIG_ATTRIBUTE); //$NON-NLS-1$
+		}
+
+		if (inputSchemaKeyAttribute != null) {
+			Value inputSchemaKeyValue = keyFac.createValue(inputSchemaKeyAttribute.getLocalName(),
+					element);
+			datamapperMediator.setInputSchemaKey(inputSchemaKeyValue);
+		} else {
+		//	handleException(INPUTSCHEMA_ATTRIBUTE); //$NON-NLS-1$
+		}
+
+		if (outputSchemaKeyAttribute != null) {
+			Value outputSchemaKeyValue = keyFac.createValue(outputSchemaKeyAttribute.getLocalName(),
+					element);
+			datamapperMediator.setOutputSchemaKey(outputSchemaKeyValue);
+		} else {
+		//	handleException(OUTPUTSCHEMA_ATTRIBUTE); //$NON-NLS-1$
+		}
+		
+		processAuditStatus(datamapperMediator, element);
+
+		return datamapperMediator;
+	}
+}
